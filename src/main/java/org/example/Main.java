0ddce076c07f6771e5a53c3fa6e856abc04f6607
JoelBrondo <joelbrondo@gmail.com>
2023-08-05 22:45:02 -0300

    Proyecto base

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(2);
+        System.out.println(1);
+        System.out.println(3);
+
+
+    }
+}
